08c06396c4d26d899c19247f4b043036ed2061a0
Hoàng Anh Đức <69144221+anhduc1020@users.noreply.github.com>
2023-10-27 10:35:58 +0700

    Add files via upload

---
+++ kuem1020_test.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: KT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+/// @custom:security-contact kuem1020@gmail.com
+contract Kuem1020_test is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20("kuem1020_test", "kt") ERC20Permit("kuem1020_test") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
